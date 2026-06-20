dfeca625f7d3e00f7023c95b2ef3de2867ce55da
君惜 <xinlei3166@126.com>
2026-06-20 16:30:54 +0800

    chore: v

--- src/render/views/home/index.vue
+++ src/render/views/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="@/assets/logo.png" />
+  <img alt="Vue logo" src="@/assets/logo.png" style="display: inline-block" />
 </template>
 
 <script setup lang="ts"></script>
